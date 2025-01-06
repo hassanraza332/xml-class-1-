4e6815bb4bf31543b623208ad8655a5970415be7
hassanraza332 <hydracare042@gmail.com>
2025-01-06 04:03:16 -0800

    js class 1

---
+++ script.js
@@ -0,0 +1,36 @@
+//var a = 3;
+// a += a;
+//console.log(a);
+
+//a=3;
+//b=3;
+//console.log(a===b)
+ 
+
+//var mode = "dark"
+//var color;
+//if(mode==="dark")
+//{
+  //  color="black"
+//}
+//else if(mode ==="light")
+//{
+
+  //  color="white"
+//}
+//else {
+
+  //  color = "blue"//
+//}
+//console.log(color)
+
+//var a =prompt ("enter a number");
+//if(a/2===0)
+//{
+  //  console.log("even")
+
+//}
+//else 
+//{
+  //  console.log("odd")
+//}
