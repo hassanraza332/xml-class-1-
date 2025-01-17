d73eaa2f38c2d6fef7608dd4b4b42226d188ce89
hassanraza332 <hydracare042@gmail.com>
2025-01-17 04:03:49 -0800

    hhhh

---
+++ js5.js
@@ -0,0 +1,68 @@
+ //let regex = /^(0)[0-9]{10}  $ /g   ; 
+//let a = "03306859954";
+//if (regex.test(a))
+//{
+//console.log(true)
+
+//}
+//else{
+  //  console.log(false)
+//}
+
+
+
+//oop
+
+//class person
+//{
+
+  //  constructor(name,age)
+    //{
+     //   this.sname=name;
+       // this.sage=age;
+    //}
+
+    //message(a)
+    //{
+      //  console.log("my name is",this.sname,"my age is",this.sage)
+    //}
+    //static sum ()
+    //{
+      //  console.log("this is static method")
+    //}
+//}
+//let obj = new person("ahsan",23);
+//obj.message("")
+//person.sum();
+
+//class car
+//{
+//constructor(brand,model,year)
+//{
+//this.cbrand=brand ;
+//this.cmodel=model ;
+//this.cyear=year ;
+
+//}
+//message()
+//{
+
+   // console.log("this car is a product of",this.cbrand,"the version of the car is",this.cmodel,"the manufacture year is",this.cyear)
+
+
+//}
+//static getdetails()
+//{
+
+//console.log("mazda rx8????? ")
+
+
+
+//}
+
+
+
+//}
+//let obj = new car ("mazda","rx8",2020);
+//obj.message();
+//car.getdetails();
